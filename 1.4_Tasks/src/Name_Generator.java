127d7efa611f218bf9871b10ffb137ce7ca88289
viettranni <vttranviett@gmail.com>
2024-10-24 15:04:36 +0300

    Task 1: Generating random names

---
+++ 1.4_Tasks/src/Name_Generator.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Name_Generator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] firstNames = {"Viet", "Saed", "Pekka", "Trung", "Matti"};
+        String[] lastNames = {"Tran", "Abukar", "Le", "Mattinen", "Nguyen"};
+
+        System.out.println("How many random names would you like to generate?");
+        int randomNameAmount = scanner.nextInt();
+
+        for ( int i = 0; i < randomNameAmount; i++) {
+            String randomFirstName = firstNames[(int) (Math.random() * firstNames.length)];
+            String randomLastName = lastNames[(int) (Math.random() * firstNames.length)];
+            String completeName = randomFirstName + " " + randomLastName;
+            System.out.println(completeName);
+        }
+    }
+}
