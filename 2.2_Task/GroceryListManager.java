59fec0dbb03d12ececb0022f9fa4ed2f905b8e09
viettranni <vttranviett@gmail.com>
2024-11-03 20:04:34 +0200

    The first task

---
+++ 2.2_Task/GroceryListManager.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+
+public class GroceryListManager {
+    private ArrayList<String> groceryList = new ArrayList<>();
+
+    public void addItem(String item) {
+        groceryList.add(item);
+        System.out.println("The " + item + " added to the list successfully");
+    }
+
+    public void removeItem(String item) {
+        if (groceryList.contains(item)) {
+            groceryList.remove(item);
+            System.out.println("Removed the " + item + " successfully");
+        } else {
+            System.out.println("Removing failed, the item is not in the basket!");
+        }
+    }
+
+    public void displayList() {
+        for (String item : groceryList) {
+            System.out.println(item);
+        }
+    }
+
+    public boolean checkItem(String item) {
+        if (groceryList.contains(item)) {
+            System.out.println( "The grocery list does contain the " + item );
+            return true;
+        } else {
+            System.out.println("The grocery list doesn't include the " + item );
+            return false; 
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to our Grocery List!");
+        System.out.println("Let's add a few items to the grocery list: ");
+
+        GroceryListManager groceryList = new GroceryListManager();
+
+        // Adding some random items
+        groceryList.addItem("Apple");
+        groceryList.addItem("Pear");
+        groceryList.addItem("Ananas");
+
+        // Displaying the entire list
+        groceryList.displayList();
+
+        // Checking if the apple is in the list
+        groceryList.checkItem("Apple");
+
+        // Removing an item from the list
+        groceryList.removeItem("Pear");
+
+        // Displaying the list again
+        groceryList.displayList();
+
+    }
+}
