8cf69460c70c909ff4f8df7007c9889b683dc5e4
viettranni <vttranviett@gmail.com>
2024-11-03 20:40:37 +0200

    The second task

--- 2.2_Task/GroceryListManager.java
+++ 2.2_Task/GroceryListManager.java
@@ -1,19 +1,19 @@
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class GroceryListManager {
-    private ArrayList<String> groceryList = new ArrayList<>();
+    private HashMap<String, Double> groceryList = new HashMap<>();
 
-    public void addItem(String item) {
-        if (groceryList.contains(item)) {
+    public void addItem(String item, Double price) {
+        if (groceryList.containsKey(item)) {
             System.out.println("The item is duplicate, we won't be adding the item!");
         } else {
-            groceryList.add(item);
+            groceryList.put(item, price);
             System.out.println("The " + item + " added to the list successfully");
         }
     }
 
     public void removeItem(String item) {
-        if (groceryList.contains(item)) {
+        if (groceryList.containsKey(item)) {
             groceryList.remove(item);
             System.out.println("Removed the " + item + " successfully");
         } else {
@@ -23,19 +23,30 @@ public class GroceryListManager {
 
     public void displayList() {
         System.out.println("Opening the Grocery List:");
-        for (String item : groceryList) {
-            System.out.println(item);
+        for (String item : groceryList.keySet()) {
+            System.out.println(item + ": $" + groceryList.get(item));
         }
     }
 
     public boolean checkItem(String item) {
-        if (groceryList.contains(item)) {
+        if (groceryList.containsKey(item)) {
             System.out.println( "The grocery list does contain the " + item );
             return true;
         } else {
             System.out.println("The grocery list doesn't include the " + item );
             return false; 
         }
+    }
+
+    public void calculateTotalCost() {
+        double totalCost = 0;
+
+        for ( String getPrice : groceryList.keySet()) {
+            double cost = groceryList.get(getPrice);
+            totalCost += cost;
+        }
+
+        System.out.println("The total cost of the List: $" + totalCost);
     }
 
     public static void main(String[] args) {
@@ -45,21 +56,25 @@ public class GroceryListManager {
         GroceryListManager groceryList = new GroceryListManager();
 
         // Adding some random items
-        groceryList.addItem("ironman");
-        groceryList.addItem("hulk");
-        groceryList.addItem("ironman");
+        groceryList.addItem("ironman", 500.52);
+        groceryList.addItem("hulk", 42.21);
+        groceryList.addItem("captain", 63.23);
 
         // Displaying the entire list
         groceryList.displayList();
 
         // Checking if the apple is in the list
-        groceryList.checkItem("Apple");
+        groceryList.checkItem("ironman");
 
         // Removing an item from the list
-        groceryList.removeItem("Pear");
+        groceryList.removeItem("captain");
 
         // Displaying the list again
         groceryList.displayList();
+
+        // Calculating the total cost
+        System.out.println("Calculating the total cost...");
+        groceryList.calculateTotalCost();
 
     }
 }
