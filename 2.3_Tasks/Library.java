d176ed715254c15249210132265d7961618ac515
viettranni <vttranviett@gmail.com>
2024-11-04 23:54:41 +0200

    Task 2

--- 2.3_Tasks/Library.java
+++ 2.3_Tasks/Library.java
@@ -31,4 +31,29 @@ public class Library {
         }
     }
 
+    public void borrowBook(String title) {
+        System.out.println("Welcome to our Library! You came to borrow a book.");
+
+        // Checking if the book is available
+        for (int i = 0; i < books.size(); i++) {
+            if (books.get(i).getTitle().equals(title)) {
+                books.remove(i);
+                System.out.println("The book " + books.get(i).getTitle() + " was borrowed successfully!");
+                break; // Exits the loop when wound
+            }
+        }
+    }
+
+    public void returnBook(Book book) {
+        System.out.println("Welcome to our Library! You came to return the book.");
+        System.out.println("Which book would you like to return?");
+        // Check if the book is already in the library
+        if (books.contains(book)) {
+            System.out.println("This book is already in the library, you can't return it.");
+            return; // Exit if the book is already present
+        }
+        books.add(book);
+        System.out.println("You have returned the book: " + book.getTitle());
+    }
+
 }
--- 2.3_Tasks/LibraryMain.java
+++ 2.3_Tasks/LibraryMain.java
@@ -3,16 +3,29 @@ public class LibraryMain {
         Book firstBook = new Book("Five Things", "Viet", 2010);
         Book secondBook = new Book("Pumpking", "Ivan", 2020);
         Book thirdBook = new Book("Cat and Mouse", "Saed", 2018);
+        Book fourthBook = new Book("Micky Moo", "Pavel", 1999);
 
         Library turkuLibrary = new Library();
 
+        // Adding books to the library
         turkuLibrary.addBook(firstBook);
         turkuLibrary.addBook(secondBook);
         turkuLibrary.addBook(thirdBook);
 
+        // Displaying all books
         turkuLibrary.displayBooks();
 
+        // Finding books by specific authors
         turkuLibrary.findBooksByAuthor("Viet");
         turkuLibrary.findBooksByAuthor("someone");
+
+        // Borrowing a book
+        turkuLibrary.borrowBook("Five Things");
+
+        // Returning a different book (for demonstration)
+        turkuLibrary.returnBook(fourthBook);
+
+        // Displaying books after borrowing and returning
+        turkuLibrary.displayBooks();
     }
 }
