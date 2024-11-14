c5e7c74317c89529bc7aead7ed6d6bc45c037cac
viettranni <vttranviett@gmail.com>
2024-11-14 17:06:48 +0200

    Adding comments to the code.

--- task4_2/Calculator.java
+++ task4_2/Calculator.java
@@ -1,15 +1,18 @@
 public class Calculator {
     private int currentValue;
 
+    // Constructor to initialize the calculator
     public Calculator() {
         this.currentValue = 0;
     }
 
+    // Method to reset the calculator to zero
     public void reset() {
         this.currentValue = 0;
     }
 
-
+    // Method to add a positive integer to the calculator
+    // Throws an exception if the integer is negative
     public void add(int number) throws IllegalArgumentException {
         if (number < 0) {
             throw new IllegalArgumentException("Negative integers are not allowed.");
@@ -17,13 +20,16 @@ public class Calculator {
         this.currentValue += number;
     }
 
+    // Method to get the current value of the calculator
     public int getCurrentValue() {
         return this.currentValue;
     }
 
+    // Temporary main method to demonstrate the use of the Calculator class
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         
+        // Test the methods
         try {
             System.out.println("Initial value: " + calc.getCurrentValue());
             calc.add(10);
@@ -31,6 +37,8 @@ public class Calculator {
             calc.add(20);
             System.out.println("After adding 20: " + calc.getCurrentValue());
             
+            // Uncomment the line below to test exception handling
+            // calc.add(-5);  // This will throw an exception
 
             calc.reset();
             System.out.println("After reset: " + calc.getCurrentValue());
